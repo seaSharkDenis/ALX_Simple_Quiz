6cf6320b0d82c59b875e15d1037bbb31c19dcb06
Denis Ndiritu <denisndiritu1@gmail.com>
2025-06-13 00:01:17 +0300

    Created a simple quiz and added an event listener to the submit button and provided output to the user

---
+++ quiz.js
@@ -0,0 +1,17 @@
+// Function checks is the user's selected answer is correct
+// and displays appropriate feedback.
+
+function checkAnswer(){
+    let feedback = document.getElementById("feedback");
+    let correctAnswer = "4";
+    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    if(userAnswer == correctAnswer){
+        feedback.textContent = "Correct! Well done.";
+    }else{
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
+
+// Add an event listener to the submit buton
+let submitAnswer = document.getElementById("submit-answer");
+submitAnswer.addEventListener("click", checkAnswer);
