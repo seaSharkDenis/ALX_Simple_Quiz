374ba4da05cb7cc6b61e43dafa717b55838df004
Denis Ndiritu <denisndiritu1@gmail.com>
2025-06-13 00:58:16 +0300

    created a simple calculator that returns the result for addition, subtraction, division and mutlipliction operations

--- quiz.js
+++ quiz.js
@@ -1,15 +1,15 @@
 // Function checks is the user's selected answer is correct
 // and displays appropriate feedback.
 
-function checkAnswer(){
-    let feedback = document.getElementById("feedback");
-    let correctAnswer = "4";
-    let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-    if (userAnswer === correctAnswer){
-        feedback.textContent = "Correct! Well done.";
-    }else{
-        feedback.textContent = "That's incorrect. Try again!";
-    }
+function checkAnswer() {
+  let feedback = document.getElementById("feedback");
+  let correctAnswer = "4";
+  let userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+  } else {
+    feedback.textContent = "That's incorrect. Try again!";
+  }
 }
 
 // Add an event listener to the submit buton
